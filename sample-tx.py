7be641fcd67a1201573872fc2607b567ba6617a2
Firat Kasmis <firat.kasmis@nfiniity.com>
2024-08-23 14:11:55 +0200

    add python v2x example

---
+++ sample-tx.py
@@ -0,0 +1,40 @@
+import sys
+sys.path.append("build")
+
+import socket
+from nfiniity_cube_radio_pb2 import *
+
+class CubeEvk:
+    def __init__(self, cube_ip):
+        self.cube_port_tx = 33210
+        self.cube_ip = cube_ip
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def send_data(self, data):
+        msg = LinkLayerTransmission()
+        msg.source = bytes([0x00, 0x00, 0x00, 0x00, 0x00, 0x00])
+        msg.destination = bytes([0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF])
+        msg.priority = BEST_EFFORT
+        msg.payload = data
+
+        cmd = CommandRequest()
+        cmd.linklayer_tx.CopyFrom(msg)
+        
+        self.sock.sendto(cmd.SerializeToString(), (self.cube_ip, self.cube_port_tx))
+
+    def close(self):
+        self.sock.close()
+
+def main():
+    cube_ip = "127.0.0.1"
+    cube = CubeEvk(cube_ip)
+
+    try:
+        data = "hello v2x" # we need minimum data of 2 bytes
+        cube.send_data(data.encode())
+
+    finally:
+        cube.close()
+
+if __name__ == "__main__":
+    main()
